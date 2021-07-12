663ef022c505240641fe00fe816364e9fbabc9aa
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-07-12 12:40:38 +0700

    add home page and add some components

---
+++ components/back.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="flex text-blue-500">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
+            </svg>
+            <NuxtLink class="ml-3" :to="to">Back</NuxtLink>
+        </div>
+        <br/>
+        <hr/>
+        <br/>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Back',
+        props: {
+            to: {
+                type: String,
+                default: () => "/"
+            },
+        },
+    }
+</script>
---
+++ components/card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+        <div>
+            <h2 class="text-gray-800 text-2xl font-semibold">{{ title }}</h2>
+            <p class="mt-2 text-gray-600">{{ description }}</p>
+            <br>
+            <hr>
+            <br>
+            <div class="text-right">
+                <NuxtLink :to="to" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Try this
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Card',
+        props: [
+            'title',
+            'description',
+            'to',
+        ]
+    }
+</script>
