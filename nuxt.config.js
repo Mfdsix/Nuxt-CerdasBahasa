983a8d52a336b6894333525a3949c801cbbde934
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-07-12 11:32:15 +0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,74 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'CerdasBahasa',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://axios.nuxtjs.org/
+    '@nuxtjs/axios'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    proxy: true,
+    credential: false
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  // axios proxy
+  proxy: {
+    '/api-text': {
+      target: 'https://baconipsum.com/api/?type=all-meat'
+    },
+    '/api-sentiment': {
+      target: 'https://sentim-api.herokuapp.com/api/v1/'
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="w-full max-w-md m-auto mt-10">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+          Type text to analyze
+        </label>
+        <textarea v-model="model.text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" rows="10" type="text" placeholder="Type text to analyze"></textarea>
+      </div>
+      <div class="flex items-center justify-between mb-4">
+        <button v-if="!loading.generate" @click="getText" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Generate Text
+        </button>
+        <button v-else disabled class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          loading ...
+        </button>
+        <button v-if="!loading.analyze" @click="analyzeText" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Analyze
+        </button>
+        <button v-else disabled class="disabled bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          loading ...
+        </button>
+      </div>
+      <hr>
+      <div class="text-center mt-4" v-if="result.type">
+        <h3 class="mb-2">Result</h3>
+        <div :class="result.bg + ' w-auto m-auto text-white p-10'">
+          <p class="text-4xl font-bold">{{ result.score }}</p>
+          <i>{{ result.type }}</i>
+        </div>
+        
+        <div class="overflow-hidden bg-white rounded w-full border mt-4 text-left">
+          <div class="block group hover:bg-blue p-4 border-b" v-for="(sentence, i) in result.sentences" :key="i">
+            <p v-if="sentence.sentiment.type == 'negative'" class="font-bold text-lg mb-1 text-red-500">Negative</p>
+            <p v-if="sentence.sentiment.type == 'neutral'" class="font-bold text-lg mb-1 text-yellow-500">Neutral</p>
+            <p v-if="sentence.sentiment.type == 'positive'" class="font-bold text-lg mb-1 text-green-500">Positive</p>
+            <p class="text-grey-darker mb-2">{{ sentence.sentence }}</p>
+          </div>
+        </div>
+      </div>
+    </form>
+    <p class="text-center text-gray-500 text-xs">
+      &copy;2021 CerdasBahasa. All rights reserved.
+    </p>
+  </div>
+</template>
+
+<script>
+  
+  const bgResult = {
+    'negative': 'bg-red-500',
+    'neutral': 'bg-yellow-500',
+    'positive': 'bg-green-500'
+  }
+  
+  export default {
+    name: 'Index',
+    data(){
+      return{ 
+        model: {
+          text: null,
+        },
+        loading: {
+          generate: false,
+          analyze: false,
+        },
+        result: {
+          bg: null,
+          type: null,
+          score: 0,
+          sentences: []
+        }
+      }
+    },
+    methods: {
+      async getText(){
+        this.loading.generate = true;
+        const text = await this.$axios.$get("/api-text")
+        this.loading.generate = false;
+        
+        if(text){
+          this.model.text = text[0]
+        }
+      },
+      async analyzeText(){
+        if(!this.model.text){
+          alert("Please type text first")
+          return
+        }
+        this.result= {
+          type: null,
+          score: 0,
+          sentences: []
+        }
+        
+        this.loading.generate = true
+        this.loading.analyze = true
+        
+        const analyze = await this.$axios.$post("https://sentim-api.herokuapp.com/api/v1/", {
+          text: this.model.text
+        })
+        this.loading.generate = false
+        this.loading.analyze = false
+        
+        if(analyze){
+          this.result = {
+            bg: bgResult[analyze?.result?.type],
+            type: analyze?.result?.type,
+            score: analyze?.result?.polarity,
+            sentences: analyze?.sentences
+          }
+        }
+      }
+    }
+  }
+</script>
