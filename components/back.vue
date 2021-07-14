0f71e716c7ff71a2bc8dfa013a32d16255bf51fb
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-07-14 13:09:25 +0700

    fix back component

--- components/back.vue
+++ components/back.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        <div class="flex text-blue-500">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
-            </svg>
-            <NuxtLink class="ml-3" :to="to">Back</NuxtLink>
-        </div>
+        <NuxtLink :to="to">
+            <div class="flex text-blue-500">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
+                </svg>
+                <span class="ml-3">Back</span>
+            </div>
+        </NuxtLink>
         <br/>
         <hr/>
         <br/>
